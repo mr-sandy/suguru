2edece7aab18819f119a6ed9903332362a47ec19
Sandy Cormie <sandy.cormie@bbc.co.uk>
2021-08-08 16:21:15 +0100

    added useReducer and Game.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
+import Game from "./Game";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> !!!! </h1>
+        <Game />
       </div>
     );
   }
