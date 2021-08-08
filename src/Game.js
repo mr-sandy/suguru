5f322439ca6127adce321c02309e3e3974eddddd
Sandy Cormie <sandy.cormie@bbc.co.uk>
2021-08-08 17:18:00 +0100

    added initial rendering of board

--- src/Game.js
+++ src/Game.js
@@ -1,27 +1,113 @@
+import React, { useReducer } from "react";
 
-import React, { useReducer} from "react";
-
-const initialState = {count: 0};
+const initialState = {
+  count: 1,
+  cells: [
+    [
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: null, terminatesRight: false, terminatesBelow: false },
+      { value: 3, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: false },
+    ],
+    [
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: 5, terminatesRight: true, terminatesBelow: true },
+      { value: 4, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: true },
+    ],
+    [
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: 2, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: 3, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: true },
+    ],
+    [
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: false },
+    ],
+    [
+      { value: null, terminatesRight: true, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: null, terminatesRight: false, terminatesBelow: false },
+      { value: null, terminatesRight: true, terminatesBelow: false },
+      { value: null, terminatesRight: true, terminatesBelow: true },
+    ],
+    [
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: true },
+      { value: null, terminatesRight: true, terminatesBelow: true },
+      { value: 1, terminatesRight: false, terminatesBelow: true },
+      { value: null, terminatesRight: false, terminatesBelow: true },
+      { value: 2, terminatesRight: true, terminatesBelow: true },
+    ],
+  ],
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'increment':
-      return {count: state.count + 4};
-    case 'decrement':
-      return {count: state.count - 1};
+    case "increment":
+      return { count: state.count + 4 };
+    case "decrement":
+      return { count: state.count - 11 };
     default:
       throw new Error();
   }
 }
-function Counter() {
+
+const tableStyle = {
+  borderCollapse: "collapse",
+  border: "3px solid black",
+};
+
+const cellStyle = (cell) => ({
+  padding: "10px",
+  height: "20px",
+  width: "20px",
+  borderColor: "black",
+  borderStyle: "solid",
+  borderTopWidth: "1px",
+  borderLeftWidth: "1px",
+  borderRightWidth: cell.terminatesRight ? "3px" : "1px",
+  borderBottomWidth: cell.terminatesBelow ? "3px" : "1px",
+  margin: 0,
+});
+
+function Game() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { cells } = state;
+
   return (
     <>
-      Count: {state.count}
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <table className="game" style={tableStyle}>
+        <tbody>
+          {cells.map((row, i) => {
+            return (
+              <tr key={i}>
+                {row.map((cell, j) => {
+                  return (
+                    <td key={j} style={cellStyle(cell)}>
+                      {cell.value}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
 
-export default Counter;
+export default Game;
