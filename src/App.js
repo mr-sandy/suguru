a605e8346fc1a6d4684d98284b6ee86be29cec6b
Sandy Cormie <sandy.cormie@bbc.co.uk>
2021-08-09 12:18:10 +0100

    updated webpack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
-import "./App.css";
+// import "./App.css";
 import NewGame from "./NewGame";
 
 class App extends Component{
